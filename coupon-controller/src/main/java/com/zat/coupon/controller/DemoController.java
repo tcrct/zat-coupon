63126bcac825462e94ac43e82c9ed4d7731a0882
tcrct <tcrct@163.com>
2020-05-29 19:40:52 +0800

    持续开发

--- coupon-controller/src/main/java/com/zat/coupon/controller/DemoController.java
+++ coupon-controller/src/main/java/com/zat/coupon/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.zat.coupon.controller;
 import com.zat.coupon.entity.User;
 import com.zat.coupon.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
@@ -40,6 +41,8 @@ public class DemoController {
     @ResponseBody
     public String save1(@Validated @RequestBody User user) {
         System.out.println("####################");
+        user.setName(null);
+        java.util.Objects.requireNonNull(user.getName());
         User user1 =  demoService.save(user);
         return user1.getName();
     }
--- coupon-web/src/main/java/com/zat/coupon/web/Application.java
+++ coupon-web/src/main/java/com/zat/coupon/web/Application.java
@@ -2,8 +2,10 @@ package com.zat.coupon.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.zat.coupon.*","com.springboot.side.duang.exception"})
+//@ComponentScan(basePackages = {"com.zat.coupon.*","com.springbootside.duang.*"})
 public class Application {
 
 	public static void main(String[] args) {
